9288824bc1d6e0079c1b9006c1ac89e811a2b042
Javad Faghih <Javad.4145@gmail.com>
2025-01-01 09:24:16 +0300

    Initial Commit

---
+++ Movie/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp.swift
+//  Movie
+//
+//  Created by Javad Faghih on 1/1/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MovieTests/MovieTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTests.swift
+//  MovieTests
+//
+//  Created by Javad Faghih on 1/1/25.
+//
+
+import Testing
+@testable import Movie
+
+struct MovieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
