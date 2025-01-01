746f781a1b3a8c2dddc6982cad3d01e8a0fe1a7e
Javad Faghih <Javad.4145@gmail.com>
2025-01-01 15:44:49 +0300

    test added for movie end point

--- MovieTests/MovieTests.swift
+++ MovieTests/MovieTests.swift
@@ -5,13 +5,46 @@
 //  Created by Javad Faghih on 1/1/25.
 //
 
-import Testing
+import XCTest
 @testable import Movie
 
-struct MovieTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+final class EndPointSuccessTests: XCTestCase {
+    
+    func testFetchMoviesEndpointProperties() throws {
+        // Arrange
+        let movieName = "Inception"
+        let endpoint = MovieEndPoints.fetchMovies(name: movieName)
+        
+        // Act & Assert
+        
+        // Test scheme
+        XCTAssertEqual(endpoint.scheme, MovieApiInfo.scheme, "The scheme should match the value in MovieApiInfo.")
+        
+        // Test host
+        XCTAssertEqual(endpoint.host, MovieApiInfo.host, "The host should match the value in MovieApiInfo.")
+        
+        // Test path
+        XCTAssertEqual(endpoint.path, "/3/search/movie", "The path should be '/3/search/movie' for fetchMovies.")
+        
+        // Test HTTP Method
+        XCTAssertEqual(endpoint.HTTPMethod, .GET, "The HTTP method for fetchMovies should be GET.")
+        
+        // Test header
+        XCTAssertEqual(endpoint.header?["Content-Type"], "application/json", "The Content-Type header should be 'application/json'.")
+        
+        // Test query parameters
+        let queryParams = endpoint.queryParams
+        XCTAssertTrue(queryParams.contains(where: { $0.name == "api_key" && $0.value == MovieApiInfo.apiKey }), "Query parameters should include the API key.")
+        XCTAssertTrue(queryParams.contains(where: { $0.name == "query" && $0.value == movieName }), "Query parameters should include the movie name as 'query'.")
+        
+        // Test additional query parameters
+        let additionalQueryParams = endpoint.additionalQueryParams
+        XCTAssertNotNil(additionalQueryParams, "Additional query parameters should not be nil.")
+        XCTAssertEqual(additionalQueryParams?.first?.name, "query", "The name of the additional query parameter should be 'query'.")
+        XCTAssertEqual(additionalQueryParams?.first?.value, movieName, "The value of the additional query parameter should match the provided movie name.")
+        
+        // Test body
+        XCTAssertNil(endpoint.body, "The body for GET requests should be nil.")
     }
-
 }
+
